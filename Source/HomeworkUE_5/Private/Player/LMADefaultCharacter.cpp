0a1ef14e5ebcdf9efa1e18169fba0b4a918dddfc
Даниил <derendervad@gmail.com>
2024-01-28 00:10:30 +0300

    initial commit

---
+++ Source/HomeworkUE_5/Private/Player/LMADefaultCharacter.cpp
@@ -0,0 +1,176 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#include "Player/LMADefaultCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/DecalComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/LMAHealthComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/Engine.h"
+
+ALMADefaultCharacter::ALMADefaultCharacter()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	SpringArmComponent->SetUsingAbsoluteRotation(true);
+	SpringArmComponent->TargetArmLength = ArmLength;
+	SpringArmComponent->SetRelativeRotation(FRotator(YRotation, 0.0f, 0.0f));
+	SpringArmComponent->bDoCollisionTest = false;
+	SpringArmComponent->bEnableCameraLag = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponent->SetupAttachment(SpringArmComponent);
+	CameraComponent->SetFieldOfView(FOV);
+	CameraComponent->bUsePawnControlRotation = false;
+
+	HealthComponent = CreateDefaultSubobject<ULMAHealthComponent>("HealthComponent");
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
+
+void ALMADefaultCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (CursorMaterial)
+	{
+		CurrentCursor = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), CursorMaterial, CursorSize, FVector(0));
+	}
+
+	OnHealthChanged(HealthComponent->GetHealth());
+	HealthComponent->OnDeath.AddUObject(this, &ALMADefaultCharacter::OnDeath);
+	HealthComponent->OnHealthChanged.AddUObject(this, &ALMADefaultCharacter::OnHealthChanged);
+}
+
+void ALMADefaultCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!(HealthComponent->IsDead()))
+	{
+		RotationPlayerOnCursor();
+	}
+
+	/*APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (PC)
+	{
+		FHitResult ResultHit;
+		PC->GetHitResultUnderCursor(ECC_GameTraceChannel1, true, ResultHit);
+		float FindRotatorResultYaw = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), ResultHit.Location).Yaw;
+		SetActorRotation(FQuat(FRotator(0.0f, FindRotatorResultYaw, 0.0f)));
+		if (CurrentCursor)
+		{
+			CurrentCursor->SetWorldLocation(ResultHit.Location);
+		}
+	}*/
+}
+
+void ALMADefaultCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ALMADefaultCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ALMADefaultCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("MoveCamera", this, &ALMADefaultCharacter::MoveCamera);
+	PlayerInputComponent->BindAxis("Sprint", this, &ALMADefaultCharacter::Sprint);
+}
+
+void ALMADefaultCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+	if (Value != 0) {
+		BaF = true;
+	} else {
+		BaF = false;
+	}
+}
+
+void ALMADefaultCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+	if (Value != 0) {
+		RaL = true;
+	} else {
+		RaL = false;
+	}
+} 
+
+void ALMADefaultCharacter::MoveCamera(float Value) {
+	ArmLength = ArmLength + Value;
+	if (ArmLength > ArmMax) {
+		ArmLength = ArmMax;
+	}
+	else if (ArmLength < ArmMin) {
+		ArmLength = ArmMin;
+	}
+	SpringArmComponent->TargetArmLength = ArmLength;
+}
+
+void ALMADefaultCharacter::Sprint(float Value) {
+	world = GetWorld();
+	if (IsValid(world) && Value <= 0 && Stamina < 100)
+	{
+		STimeNow = world->GetTimeSeconds();
+		if ((STimeNow - STimeOld) >= StaminaTime) {
+			Stamina = Stamina + 1;
+			STimeOld = STimeNow;
+			GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString::Printf(TEXT("Stamina = %f"), Stamina));
+		}
+	}
+
+	if (Stamina > 0 && Value > 0 && BaF || RaL) {
+		Shift = true;
+		Stamina = Stamina - (Value/2);
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString::Printf(TEXT("Stamina = %f"), Stamina));
+	}
+
+	if (Value <= 0 || Stamina <= 0) {
+		Shift = false;
+	}
+}
+
+void ALMADefaultCharacter::OnDeath()
+{
+	CurrentCursor->DestroyRenderState_Concurrent();
+
+	PlayAnimMontage(DeathMontage);
+
+	GetCharacterMovement()->DisableMovement();
+
+	SetLifeSpan(5.0f);
+
+	if (Controller)
+	{
+		Controller->ChangeState(NAME_Spectating);
+	}
+}
+
+void ALMADefaultCharacter::OnHealthChanged(float NewHealth)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString::Printf(TEXT("Health = %f"), NewHealth));
+}
+
+void ALMADefaultCharacter::RotationPlayerOnCursor()
+{
+	APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (PC)
+	{
+		FHitResult ResultHit;
+		PC->GetHitResultUnderCursor(ECC_GameTraceChannel1, true, ResultHit);
+		float FindRotatorResultYaw = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), ResultHit.Location).Yaw;
+		SetActorRotation(FQuat(FRotator(0.0f, FindRotatorResultYaw, 0.0f)));
+		if (CurrentCursor)
+		{
+			CurrentCursor->SetWorldLocation(ResultHit.Location);
+		}
+	}
+}
+
